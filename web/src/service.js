58f8e97f01aae2d553d3ec2633c5f6a66a486834
Greg Walker <michael.walker@gsa.gov>
2016-02-09 13:12:25 -0600

    And add auth to the client side, too

--- web/src/service.js
+++ web/src/service.js
@@ -2,6 +2,18 @@ const request = require("browser-request");
 const dispatcher = require("./dispatcher");
 
 module.exports = {
+	getUser() {
+		request.get("/api/user", (err, res) => {
+			if(!err && res.body) {
+				try {
+					dispatcher.dispatch({
+						type: "user-in",
+						payload: JSON.parse(res.body)
+					});
+				} catch(e) { }
+			}
+		});
+	},
 	getFlights() {
 		request.get("/api/flights", (err, res) => {
 			if(!err && res.body) {
---
+++ web/src/stores/userStore.js
@@ -0,0 +1,24 @@
+const dispatcher = require("../dispatcher");
+import { Store } from "flux/utils";
+
+class UserStore extends Store {
+    constructor(dispatcher) {
+        super(dispatcher);
+        this._user = { };
+    }
+
+    getUser() {
+        return this._user;
+    }
+
+    __onDispatch(event) {
+        switch(event.type) {
+            case "user-in":
+                this._user = event.payload;
+                this.__emitChange();
+                break;
+        }
+    }
+}
+
+module.exports = new UserStore(dispatcher);
