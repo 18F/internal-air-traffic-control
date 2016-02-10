5bfe772f2618d910391cb01603661ab849d4e4b1
Greg Walker <michael.walker@gsa.gov>
2016-02-10 12:53:59 -0600

    Cache off the list feed URL since it's not likely to change

--- server/sheet.js
+++ server/sheet.js
@@ -8,38 +8,42 @@ class Sheet {
     }
 
     getListFeedURL(accessToken) {
-        return new Promise((resolve, reject) => {
-            request.get(`https://spreadsheets.google.com/feeds/worksheets/${this.id}/private/full?alt=json`, { headers: { "Authorization": `Bearer ${accessToken}` }}, (err, res, body) => {
-                if(err) {
-                    return reject(err);
-                }
-
-                try {
-                    body = JSON.parse(body);
-                } catch(e) {
-                    return reject(new Error("Could not parse sheet body"));
-                }
-
-                if(!body.feed || !body.feed.entry || !body.feed.entry[0]) {
-                    return reject(new Error("Invalid sheet"));
-                }
-
-                for(let link of body.feed.entry[0].link) {
-                    if(link.rel.indexOf("#listfeed") >= 0) {
-                        return resolve(link.href)
+        if(!this.listFeedURL) {
+            return new Promise((resolve, reject) => {
+                request.get(`https://spreadsheets.google.com/feeds/worksheets/${this.id}/private/full?alt=json`, { headers: { "Authorization": `Bearer ${accessToken}` }}, (err, res, body) => {
+                    if(err) {
+                        return reject(err);
                     }
-                }
 
-                reject(new Error("Could not find list feed link"));
+                    try {
+                        body = JSON.parse(body);
+                    } catch(e) {
+                        return reject(new Error("Could not parse sheet body"));
+                    }
+
+                    if(!body.feed || !body.feed.entry || !body.feed.entry[0]) {
+                        return reject(new Error("Invalid sheet"));
+                    }
+
+                    for(let link of body.feed.entry[0].link) {
+                        if(link.rel.indexOf("#listfeed") >= 0) {
+                            this.listFeedURL = link.href;
+                            return resolve(link.href)
+                        }
+                    }
+
+                    reject(new Error("Could not find list feed link"));
+                });
             });
-        });
+        } else {
+            return Promise.resolve(this.listFeedURL);
+        }
     }
 
     getRows(accessToken) {
         return new Promise((resolve, reject) => {
             this.getListFeedURL(accessToken)
                 .then(listFeed => {
-                    console.log(listFeed);
                     request.get(`${listFeed}?alt=json`, { headers: { "Authorization": `Bearer ${accessToken}` } }, (err, res, body) => {
                         if(err) {
                             return reject(e);
