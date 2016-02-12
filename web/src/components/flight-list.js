c7b11ab04f623d837a7f92d64cb2e17a6a411b93
Greg Walker <michael.walker@gsa.gov>
2016-02-11 18:10:55 -0600

    Add react-select component Set HTML5 doctype and UTF-8 charset Add US WDS image assets

--- web/src/components/flight-list.js
+++ web/src/components/flight-list.js
@@ -1,10 +1,13 @@
 const React = require("react");
 const Flight = require("./flight");
+const ReactSelect = require("react-select");
 const flightStore = require("../stores/flightStore");
 
 module.exports = React.createClass({
     getInitialState() {
         return {
+            visibleStatuses: [ ],
+            allStatuses: [ ],
             flights: flightStore.getFlights()
         };
     },
@@ -17,8 +20,20 @@ module.exports = React.createClass({
         this.flightStoreListenerToken.remove();
     },
 
+    statusesChanged(selected) {
+        this.setState({ visibleStatuses: selected });
+    },
+
     _storeChanged() {
-        this.setState({ flights: flightStore.getFlights() });
+        const allStatuses = [ ];
+        const flights = flightStore.getFlights();
+
+        for(let flight of flights) {
+            if(allStatuses.indexOf(flight.status) < 0) {
+                allStatuses.push(flight.status);
+            }
+        }
+        this.setState({ allStatuses, flights });
     },
 
     render() {
@@ -26,6 +41,7 @@ module.exports = React.createClass({
             <div>
                 <div className="usa-grid">
                     <h3 className="usa-width-one-whole">Flights on the Board</h3>
+                    <ReactSelect multi={true} value={ this.state.visibleStatuses } delimiter=":" onChange={this.statusesChanged} placeholder="Show statuses..." options={ this.state.allStatuses.map(status => { return { value: status, label: status }}) } />
                 </div>
                 { this.state.flights.map(flight => <Flight key={flight.id} flight={flight} />) }
             </div>
