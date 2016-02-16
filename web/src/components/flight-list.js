771265f6cf890703a967f083baca2dbf1715e1d5
Greg Walker <michael.walker@gsa.gov>
2016-02-16 15:53:09 -0600

    Fixed from a rebase loss :(

--- web/src/components/flight-list.js
+++ web/src/components/flight-list.js
@@ -33,17 +33,17 @@ module.exports = React.createClass({
         this.flightStoreListenerToken.remove();
     },
 
-    getVisibleFlights(flights, visibleStatuses, searchText) {
-        let visibleFlights = [ ];
+    getVisibleFlights(flights, visibleStatuses, searchText = "") {
+        let visibleFlights = flights.slice(0);
         if(visibleStatuses) {
             const statusNames = visibleStatuses.map(vs => vs.value);
-            return flights.filter(f => (statusNames.indexOf(f.status.toLowerCase()) >= 0));
+            visibleFlights = flights.filter(f => (statusNames.indexOf(f.status.toLowerCase()) >= 0));
         }
 
         const majorSearchProperties = [ "description", "status", "lead", "pair" ];
 
         searchText = searchText.trim().toLowerCase();
-        if(visibleFlights.length && searchText.trim()) {
+        if(visibleFlights.length && searchText.length) {
             let terms = searchText.split(" ");
             visibleFlights = visibleFlights.filter(flight => {
                 for(let term of terms) {
@@ -61,7 +61,6 @@ module.exports = React.createClass({
                 return false;
             });
         }
-
         return visibleFlights;
     },
 
@@ -72,6 +71,10 @@ module.exports = React.createClass({
     _onStatusesChanged(selected) {
         this.setState({ visibleStatuses: selected, visibleFlights: this.getVisibleFlights(this.state.flights, selected, this.state.searchField) });
         localStorage(STORAGE_KEY, selected);
+    },
+
+    _onSearchChanged(event) {
+        this.setState({ searchField: event.target.value, visibleFlights: this.getVisibleFlights(this.state.flights, this.state.visibleStatuses, event.target.value) });
     },
 
     _storeChanged() {
@@ -90,7 +93,10 @@ module.exports = React.createClass({
             <div>
                 <div className="usa-grid">
                     <h3 className="usa-width-one-whole">Flights on the Board</h3>
-                    <ReactSelect multi={true} value={ this.state.visibleStatuses } delimiter=":" onChange={this._onStatusesChanged} placeholder="Show statuses..." options={ getAllStatusObjects() } />
+                    <ReactSelect className="usa-width-three-fourths" multi={true} value={ this.state.visibleStatuses } delimiter=":" onChange={this._onStatusesChanged} placeholder="Show statuses..." options={ getAllStatusObjects() } />
+                    <div className="usa-width-one-fourth flight-list-search">
+                        <input type="text" onChange={ this._onSearchChanged } placeholder="Filter..." />
+                    </div>
                 </div>
                 <br/>
                 { this.state.visibleFlights.map(flight => <Flight key={flight.id} flight={flight} />) }
