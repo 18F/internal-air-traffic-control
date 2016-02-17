6c9d65d05da58431d9ef782a0d34c8290cec097a
Greg Walker <michael.walker@gsa.gov>
2016-02-17 14:38:07 -0600

    If only one value has changed, just update the cell rather than the row.  Better accountability?  Closes #10.

--- server/sheet.js
+++ server/sheet.js
@@ -7,6 +7,27 @@ class Sheet {
         return Promise.resolve(this.id);
     }
 
+    objDiff(o1, o2) {
+        const diff = { };
+        for(let p of Object.keys(o1)) {
+            if(o1[p] != o2[p] && p[0] != "_") {
+                diff[p] = true;
+            }
+        }
+        for(let p of Object.keys(o2)) {
+            if(o1[p] != o2[p] && p[0] != "_") {
+                diff[p] = true;
+            }
+        }
+
+        const diffProps = [ ];
+        for(let p of Object.keys(diff)) {
+            diffProps.push(p);
+        }
+
+        return diffProps;
+    }
+
     getFeedURL(accessToken, feedRel) {
         if(!this[feedRel]) {
             // If we haven't gotten the feed URL yet, go get it.
@@ -104,28 +125,101 @@ class Sheet {
 
     updateRow(row, accessToken) {
         return new Promise((resolve, reject) => {
+            let targetRow = null;
+            let cellsToChange = null;
+
+            row.staff3 = row.staff4 = "";
+            if(row.staff.length) {
+                row.staff3 = row.shift();
+                if(row.staff.length) {
+                    row.staff = row.shift();
+                }
+            }
+            delete row.staff;
+
             // Get the rows as Google has them now.  The reason for this is
             // that the edit link may have changed, if someone else has
             // modified the row since we originally fetched it.
             this.getRows(accessToken)
                 .then(rows => {
                     // Now get the row we're actually interested in.
-                    const targetRow = rows.find(r => r._id === row._id);
+                    targetRow = rows.find(r => r._id === row._id);
                     if(targetRow) {
+                        cellsToChange = this.objDiff(row, targetRow);
+                        if(cellsToChange.length == 1) {
+                            return this.getFeedURL(accessToken, "http://schemas.google.com/spreadsheets/2006#cellsfeed");
+                        }
+                        return false;
+                    } else {
+                        throw new Error(`No row found with ID [${row._id}]`);
+                    }
+                })
+                .then(cellFeedURL => {
+                    if(cellFeedURL) {
+                        return new Promise((cellUpdateResolve, cellUpdateReject) => {
+                            request.get(`${cellFeedURL}?alt=json`, { headers: { "Authorization": `Bearer ${accessToken}` } }, (err, res, body) => {
+                                if(err) {
+                                    console.log(err);
+                                    return cellUpdateReject(err);
+                                }
+
+                                try {
+                                    body = JSON.parse(body);
+                                } catch(e) {
+                                    console.log("Body parse fail");
+                                    return cellUpdateReject(e);
+                                }
+
+                                let colNumber = -1;
+                                for(let cell of body.feed.entry) {
+                                    if(cell["gs$cell"].row == 1 && cell.content["$t"].toLowerCase().replace(" ", "") == cellsToChange[0]) {
+                                        colNumber = cell["gs$cell"].col;
+                                        break;
+                                    }
+                                }
+
+                                const targetCell = body.feed.entry.find(c => (c["gs$cell"].row == targetRow._rowNumber && c["gs$cell"].col == colNumber));
+                                if(targetCell) {
+                                    const cellID = targetCell.id.$t;
+                                    const editURL = targetCell.link.find(l => l.rel == "edit");
+
+                                    request.put(editURL.href, { headers: { "Content-type": "application/atom+xml", "Authorization": `Bearer ${accessToken}` }, body: this.atomizeCell(cellID, targetRow._rowNumber, colNumber, row[cellsToChange[0]]) }, (err) => {
+                                        if(err) {
+                                            console.log("Error updating cell");
+                                            console.log(err);
+                                            return cellUpdateReject(err);
+                                        }
+                                        cellUpdateResolve();
+                                    });
+                                } else {
+                                    throw new Error(`Could not find target cell (row ${targetRow._rowNumber}, cell ${targetCell})`);
+                                }
+                            });
+                        });
+                    } else if(cellsToChange.length > 1) {
                         request.put(targetRow._links.edit, { headers: { "Content-type": "application/atom+xml", "Authorization": `Bearer ${accessToken}` }, body: this.atomizeRow(row) }, (err) => {
                             if(err) {
                                 console.log(err);
-                                reject(new Error("Error updating row"));
-                            } else {
-                                resolve();
+                                throw err;
                             }
                         });
-                    } else {
-                        reject(new Error(`No row found with ID [${row._id}]`))
                     }
-
                 })
+                .then(resolve)
+                .catch(err => {
+                    console.log("Error updating cell/row");
+                    console.log(err);
+                    reject(err);
+                });
         });
+    }
+
+    atomizeCell(id, row, column, value) {
+        return `<?xml version="1.0" encoding="UTF-8"?>
+            <entry xmlns="http://www.w3.org/2005/Atom" xmlns:gs="http://schemas.google.com/spreadsheets/2006">
+                <id>${id}</id>
+                <gs:cell row="${row}" col="${column}" inputValue="${value}"/>
+            </entry>`;
     }
 
     atomizeRow(row) {
