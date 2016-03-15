772d462a57272ede178f1fb667146accf909456a
Greg Walker <michael.walker@gsa.gov>
2016-03-15 16:29:33 -0500

    Remove Google-specific refresh code; request the scope correctly (not the scop)

--- server/auth/trello.js
+++ server/auth/trello.js
@@ -12,7 +12,7 @@ module.exports = {
       callbackURL: process.env.TRELLO_CALLBACK_URL,
       passReqToCallback: true,
       trelloParams: {
-        scop: "read,write",
+        scope: "read,write",
         name: "AirTraffic Control",
         expiration: "30days"
       }
@@ -35,48 +35,7 @@ module.exports = {
     server.get('/auth/trello/callback',
       passport.authenticate('trello', { failureRedirect: errorURL }),
       (req, res) => {
-        console.log('YAY HERE');
         res.redirect('/');
       });
   },
-
-  refresh: function(req) {
-    return new Promise((resolve, reject) => {
-      if(!req.user || !req.user.refreshToken) {
-        return reject(new Error('No user or refresh token'));
-      }
-
-      if(req.user.expires > Date.now()) {
-        resolve();
-      } else {
-        request.post('https://www.googleapis.com/oauth2/v4/token', {
-          form: {
-              'client_id': process.env.GOOG_CLIENT_ID,
-              'client_secret': process.env.GOOG_CLIENT_SECRET,
-              'refresh_token': req.user.refreshToken,
-              'grant_type': 'refresh_token'
-          } }, function(err, res, body) {
-            if(err) {
-              return reject(err);
-            }
-
-            try {
-              body = JSON.parse(body);
-            } catch(e) {
-              return reject(new Error('Could not parse refreshed token body'));
-            }
-
-            if(body.error) {
-              return reject(new Error(body.error_description));
-            }
-
-            req.user.accessToken = body.access_token;
-            req.user.expires = (Date.now() + (30 * 60 * 1000));
-            req.login(req.user, () => {
-              resolve();
-            });
-          });
-      }
-    });
-  }
 };
