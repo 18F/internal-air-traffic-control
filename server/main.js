8548ce95c9ad7e7ec4fb7a55bd05cfd481908dcf
Greg Walker <michael.walker@gsa.gov>
2016-03-15 15:40:42 -0500

    Remove the unused Google stuff; don't remove the Google auth or sheets modules just yet

--- server/main.js
+++ server/main.js
@@ -4,37 +4,33 @@ require('dotenv').config();
 const restify = require('restify');
 const passport = require('passport');
 const io = require('socket.io');
-const googleAuth = require('./auth/google');
 const trelloAuth = require('./auth/trello');
 const sessions = require('client-sessions');
-const sheet = require('./sheet');
 const board = require('./board');
 const PORT = process.env.PORT || 5000;
 const bpaTrello = require('bpa-trello-dashboard/app');
 
-if(!process.env.GOOG_CLIENT_ID) {
-  console.error('Google client ID not set.  Cannot continue.');
+if(!process.env.TRELLO_API_KEY) {
+  console.error('Trello API key not set.  Cannot continue.');
   process.exit(1);
 }
-if(!process.env.GOOG_CLIENT_SECRET) {
-  console.error('Google client secret not set.  Cannot continue.');
+if(!process.env.TRELLO_CLIENT_SECRET) {
+  console.error('Trello client secret not set.  Cannot continue.');
   process.exit(1);
 }
-if(!process.env.GOOG_CALLBACK_URL) {
-  console.error('Google callback URL not set.  Cannot continue.');
+if(!process.env.TRELLO_CALLBACK_URL) {
+  console.error('Trello callback URL not set.  Cannot continue.');
   process.exit(1);
 }
 
-if(!process.env.GOOG_SHEET_ID) {
-  console.error('Google sheet ID not set.  Cannot continue.');
+if(!process.env.TRELLO_BOARD_ID) {
+  console.error('Trello board ID not set.  Cannot continue.');
   process.exit(1);
 }
 
 if(!process.env.SESSION_SECRET) {
   console.warn('No SESSION_SECRET set.  Using less secure default.');
 }
-
-sheet.setSheetID(process.env.GOOG_SHEET_ID);
 
 const server = restify.createServer({ name: 'Traffic Control API' });
 server.use(sessions({
@@ -64,7 +60,6 @@ server.get('/auth/error', (req, res, next) => {
   return next(new restify.UnauthorizedError(''));
 });
 
-googleAuth.setupMiddleware(server, passport, '/auth/error');
 trelloAuth.setupMiddleware(server, passport, '/auth/trello');
 
 server.use((req, res, next) => {
@@ -73,15 +68,6 @@ server.use((req, res, next) => {
     next();
   } else {
     res.charSet('utf-8');
-    /*
-    googleAuth.refresh(req)
-      .then(next)
-      .catch(e => {
-        console.log('Error refreshing Google OAuth token');
-        console.error(e);
-        return next(new restify.InternalServerError('Could not refresh authentication token'));
-      });
-    */
     next();
   }
 });
@@ -106,28 +92,6 @@ server.get('/api/flights', (req, res, next) => {
             r.pair = '';
             break;
         }
-
-        /*
-        r.staff = [ ];
-        if(r.staff3 && r.staff3.length) {
-          r.staff.push(r.staff3);
-        }
-        if(r.staff4 && r.staff4.length) {
-          r.staff.push(r.staff4);
-        }
-        delete r.staff3;
-        delete r.staff4;
-
-        /*
-        setTimeout(() => {
-          cardCreator.createCard({
-            project: r.description,
-            order: '',
-            stage: r.status
-          });
-        }, delay);
-        delay += 1000;
-        */
       }
       res.send(rows);
     })
@@ -140,17 +104,6 @@ server.get('/api/flights', (req, res, next) => {
 });
 
 server.put('/api/flights', restify.bodyParser(), (req, res, next) => {
-  /*sheet.updateRow(req.body, req.user.accessToken)
-    .then(() => {
-      res.send({});
-      sockets.emit('flights changed');
-    })
-    .catch(e => {
-      console.log('Error updating flight');
-      console.log(e);
-      res.send(new restify.InternalServerError());
-    });
-  */
   res.send(new restify.InternalServerError());
   next();
 });
