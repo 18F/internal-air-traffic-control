1ef3060e0de3c8afb78105976e603efb062db495
Greg Walker <michael.walker@gsa.gov>
2016-03-16 16:30:34 -0500

    Merge flight updates rather than fetching the entire list of flights again.  Such faster.

--- web/src/stores/flightStore.js
+++ web/src/stores/flightStore.js
@@ -1,6 +1,7 @@
 'use strict';
 const dispatcher = require('../dispatcher');
 import { Store } from 'flux/utils';
+import updeep from 'updeep';
 
 class FlightStore extends Store {
   constructor(dispatcher) {
@@ -16,6 +17,22 @@ class FlightStore extends Store {
     switch(event.type) {
       case 'flights-in':
         this._flights = event.payload;
+        this.__emitChange();
+        break;
+
+      case 'flight-update':
+        for(let i = 0; i < this._flights.length; i++) {
+          if(this._flights[i]._id === event.payload._id) {
+            const update = { };
+            update[i] = event.payload;
+            const status = require('../statuses').getAll().filter(s => s.id === event.payload.listID);
+            if(status.length) {
+              update[i].status = status[0].name;
+            }
+            this._flights = updeep(update, this._flights);
+            break;
+          }
+        }
         this.__emitChange();
         break;
     }
