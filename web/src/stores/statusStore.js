bcff68083ce015f4e4cc77f845d962299b0382ce
Greg Walker <michael.walker@gsa.gov>
2016-04-04 16:05:10 -0500

    Don't need ucfirst anymore

--- web/src/stores/statusStore.js
+++ web/src/stores/statusStore.js
@@ -1,31 +1,6 @@
 const dispatcher = require('../dispatcher');
 import { Store } from 'flux/utils';
 
-/*
-const ucfirst = require('ucfirst');
-const flightStore = require('./stores/flightStore');
-
-const known = [ ];
-
-let all = [].concat(known);
-function getAll() {
-  return all;
-}
-
-flightStore.addListener(() => {
-  const flights = flightStore.getFlights();
-  all = [].concat(known);
-  for (let flight of flights) {
-    let existing = all.filter(f => f.name === flight.status);
-    if (existing.length) {
-      existing[0].id = flight.listID;
-    } else {
-      all.push({ name: flight.status, id: flight.listID });
-    }
-  }
-});
-//*/
-
 class StatusStore extends Store {
   constructor(d) {
     super(d);
