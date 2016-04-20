6041da031e1e95ad3c154af64aeb09947713eeda
Greg Walker <michael.walker@gsa.gov>
2016-04-20 15:25:42 -0500

    Add reducer for add/remove labels

--- web/src/reducers/flights.js
+++ web/src/reducers/flights.js
@@ -47,6 +47,27 @@ export default function flights(state = [], action) {
         return state;
       }
 
+    case actions.Flights.ADD_LABEL:
+      {
+        const index = state.findIndex(f => f.id === action.data.flight);
+        if (index >= 0) {
+          const update = { [index]: { labels: state[index].labels.concat(action.data.label) } };
+          return updeep(update, state);
+        }
+        return state;
+      }
+
+    case actions.Flights.REMOVE_LABEL:
+      {
+        const index = state.findIndex(f => f.id === action.data.flight);
+        if (index >= 0) {
+          const newLabels = state[index].labels.filter(l => l !== action.data.label);
+          const update = { [index]: { labels: newLabels } };
+          return updeep(update, state);
+        }
+        return state;
+      }
+
     default:
       return state;
   }
